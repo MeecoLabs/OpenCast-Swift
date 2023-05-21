78b83a1c0c60afe9568a806b974d3d64bc563986
Dustin Steiner <dustin.steiner@gmail.com>
2023-05-21 14:11:17 +0100

    add default app ids

---
+++ Sources/OpenCastKit/Control/CastAppIds.swift
@@ -0,0 +1,12 @@
+//
+//  CastAppIds.swift
+//  
+//
+//  Created by Dustin Steiner on 21.05.23.
+//
+
+import Foundation
+
+public struct CastAppIds {
+    public static let DefaultAppId = "CC1AD845"
+}
