ad8d352783bf85cec452662715af3e7fa8f3282f
Dustin Steiner <dustin.steiner@gmail.com>
2023-05-22 16:43:00 +0100

    app namespaces can be empty

--- Sources/OpenCastKit/Control/Channels/ReceiverController/ReceiverApp.swift
+++ Sources/OpenCastKit/Control/Channels/ReceiverController/ReceiverApp.swift
@@ -14,7 +14,7 @@ public struct ReceiverApp: Codable, Equatable {
     public let iconUrl: String
     public let isIdleScreen: Bool
     public let launchedFromCloud: Bool
-    public let namespaces: [ReceiverAppNamespace]
+    public let namespaces: [ReceiverAppNamespace]?
     public let sessionId: String
     public let statusText: String
     public let transportId: String
@@ -29,8 +29,11 @@ extension ReceiverApp {
         self.iconUrl = json["iconUrl"] as! String
         self.isIdleScreen = json["isIdleScreen"] as! Bool
         self.launchedFromCloud = json["launchedFromCloud"] as! Bool
-        let namespaces = json["namespaces"] as! [NSDictionary]
-        self.namespaces = namespaces.map({ ReceiverAppNamespace(json: $0) })
+        if let namespaces = json["namespaces"] as? [NSDictionary] {
+            self.namespaces = namespaces.map({ ReceiverAppNamespace(json: $0) })
+        } else {
+            self.namespaces = nil
+        }
         self.sessionId = json["sessionId"] as! String
         self.statusText = json["statusText"] as! String
         self.transportId = json["transportId"] as! String
